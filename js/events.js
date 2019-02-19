d52360aabb51c63f20112f34f61baa17631a0243
kunalvishnoi <vishnoi.kunal0@gmail.com>
2019-02-19 22:56:00 +0530

    bug fix

--- js/events.js
+++ js/events.js
@@ -1,6 +1,32 @@
+/**
+ * jQuery.deparam - The oposite of jQuery param. Creates an object of query string parameters.
+ *
+ * Credits for the idea and Regex:
+ * http://stevenbenner.com/2010/03/javascript-regex-trick-parse-a-query-string-into-an-object/
+*/
+(function($){
+  $.deparam = $.deparam || function(uri){
+    if(uri === undefined){
+      uri = window.location.search;
+    }
+    var queryString = {};
+    uri.replace(
+      new RegExp(
+        "([^?=&]+)(=([^&#]*))?", "g"),
+        function($0, $1, $2, $3) {
+          queryString[$1] = decodeURIComponent($3.replace(/\+/g, '%20'));
+        }
+      );
+      return queryString;
+    };
+})(jQuery);
+
+
 var fake_id;
 
 $(document).ready(function(){
+  var params=jQuery.deparam(window.location.search);
+  console.log(params);
   var id =localStorage.getItem('id');
   if(id==1){
     document.getElementById("event_head").src="../img/coloralo-back.svg"
